6ed729522cea3901ba3453fd87c86a30714b2f80
Tanya <voznyuk1998@mail.ru>
2022-10-14 14:44:22 +0300

    1

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/cktwhc72vvv5hd79/branch/main?svg=true)](https://ci.appveyor.com/project/TanyaVoz/ahj-homeworks-testing/branch/main)
---
+++ e2e/e2e.test.js
@@ -0,0 +1,29 @@
+import puppetteer from 'puppeteer';
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('Popover', () => {
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:8888';
+
+  beforeAll(async () => {
+    browser = await puppetteer.launch({
+      headless: false,
+      slowMo: 250,
+      devtools: true,
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('should add .active class for popover', async () => {
+    await page.goto(baseUrl);
+    const button = await page.$('[data-toggle="popover"]');
+    button.click();
+    await page.waitForSelector('[data-widget="popover-top"].active');
+  });
+});
